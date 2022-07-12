07f2c93695ac31057557e7a6fa58cc40be75ee3d
neoglez <s1039062@stud.sbg.ac.at>
2022-07-12 12:41:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 temp_dir=$(mktemp -d)
 cd "${temp_dir}"
 
-curl -L "https://github.com/neoglez/wmctrl-switch/blob/main/wmctrl-switch.sh" -o "${PWD}/wmctrl-switch.sh"
+curl -L "https://raw.githubusercontent.com/neoglez/wmctrl-switch/main/install.sh" -o "${PWD}/wmctrl-switch.sh"
 
 install -Dm755 "${temp_dir}/wmctrl-switch.sh" "/usr/local/bin/wmctrl-switch"
 
